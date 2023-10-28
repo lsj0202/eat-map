e923da9e1b421638635363bb09bd4a84957b391c
lsj0202 <w1004123@gmail.com>
2023-10-28 12:41:15 +0900

    feat: add kakao map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Layout from "@/components/Layout";
-import Link from "next/link";
-import React from "react";
+import Map from "@/components/Map";
 
-const Main = () => {
-  return (
-    <>
-      <h1>Map Index Page</h1>
-    </>
-  );
+const Home = () => {
+  return <Map />;
 };
 
-export default Main;
+export default Home;
