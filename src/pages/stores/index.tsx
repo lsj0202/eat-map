fba8026f4e20cbce941441d3595624aab53f3493
lsj0202 <w1004123@gmail.com>
2023-10-30 00:49:01 +0900

    feat: add pagenation

--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import { StoreType } from "@/interface";
+import { StoreApiResponse } from "@/interface";
 import { useQuery } from "react-query";
 import axios from "axios";
 import Loading from "@/components/Loading";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import Pagenation from "@/components/Pagenation";
 
 export default function StoreListPage() {
   const router = useRouter();
-  const { page = "1" } = router.query;
+  const { page = "1" }: any = router.query;
 
   const {
     data: stores,
@@ -16,7 +18,7 @@ export default function StoreListPage() {
   } = useQuery(`store-${page}`, async () => {
     const { data } = await axios.get(`api/stores?page=${page}`);
 
-    return data as StoreType[];
+    return data as StoreApiResponse;
   });
 
   if (isError) {
@@ -27,13 +29,15 @@ export default function StoreListPage() {
     );
   }
 
+  console.log(stores);
+
   return (
     <div className="px-4 md:max-w-4xl mx-auto py-8">
       <ul role="list" className="divide-y divide-gray-100">
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.map((store, index) => (
+          stores?.data.map((store, index) => (
             <li className="flex justify-between gap-x-6 py-5" key={index}>
               <div className="flex gap-x-4">
                 <Image
@@ -68,6 +72,9 @@ export default function StoreListPage() {
           ))
         )}
       </ul>
+      {stores?.totalPage && (
+        <Pagenation total={stores?.totalPage} page={page} />
+      )}
     </div>
   );
 }
