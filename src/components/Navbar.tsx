f554841b802bf7afa224e75602e9e30876df6007
lsj0202 <w1004123@gmail.com>
2023-11-13 09:17:33 +0900

    FIX : food img

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,62 +12,13 @@ const Navbar = () => {
         href="/"
         className="text-blue-800 text-lg font-semibold cursor-pointer px-[18px] py-0"
       >
-        NextMap
+        서울시 맛집리스트
       </Link>
       <div className="flex gap-3 items-center px-[18px] py-0 hidden-lg">
         <Link href="/stores" className="cursor-pointer hover:text-gray-500">
-          맛집 등록
+          맛집 목록
         </Link>
-        <Link href="/stores/new" className="cursor-pointer hover:text-gray-500">
-          맛집 등록
-        </Link>
-        <Link
-          href="/users/likes"
-          className="cursor-pointer hover:text-gray-500"
-        >
-          찜한 가게
-        </Link>
-        <Link
-          href="/users/login"
-          className="cursor-pointer hover:text-gray-500"
-        >
-          로그인
-        </Link>
-        {/* moblie button */}
       </div>
-      <div
-        role="presetation"
-        className="hidden cursor-pointer px-[18px] block-lg"
-        onClick={() => {
-          setIsOpen((value) => !value);
-        }}
-      >
-        {isOpen ? <AiOutlineClose /> : <BiMenu />}
-      </div>
-      {isOpen && (
-        <div className="fixed w-full top-[52px] bg-blue-800 text-white h-screen">
-          <div className="flex flex-col gap-[16px] px-[18px] py-[24px]">
-            <Link href="/stores" className="text-white">
-              맛집 등록
-            </Link>
-            <Link href="/stores/new" className="text-white hover:text-gray-300">
-              맛집 등록
-            </Link>
-            <Link
-              href="/users/likes"
-              className="text-white hover:text-gray-300"
-            >
-              찜한 가게
-            </Link>
-            <Link
-              href="/users/login"
-              className="text-white hover:text-gray-300"
-            >
-              로그인
-            </Link>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
